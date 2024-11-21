934d0733b4c611535bf5ddce84801efc88feba45
Lars Bieber <lbieber1@smail.uni-koeln.de>
2024-11-21 15:14:38 +0100

    started

--- evaluation.py
+++ evaluation.py
@@ -11,9 +11,25 @@ from sklearn.metrics import classification_report
 df = pd.read_csv("data.tsv", sep="\t")
 X = df.Abstract
 y = df.DomainID
-X_train, X_test, y_train, y_test = train_test_split(X, y)
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, stratify=y)
+
+tokenizer = Tokenizer()
+tokenizer.fit_on_texts(X_train)
+vocabSize = len(tokenizer.word_index)+1
+X_train = tokenizer.texts_to_sequences(X_train)
+
+MAX_LENGTH = max(len(X_train) for X_train in X_train)
+X_train = pad_sequences(X_train, maxlen=MAX_LENGTH, padding='post')
 
 model = Sequential()
+model.add(Embedding(vocabSize, 300, input_length=MAX_LENGTH))
+model.add(Flatten())
+model.add(Dense(64, activation="relu"))
+model.add(Dense(16, activation="relu"))
+model.add(Dense(4, activation="relu"))
+model.add(Dense(1, activation='sigmoid'))
+model.compile(loss='binary_crossentropy', optimizer='sgd')
+model.fit(X_train, y_train, epochs=10, verbose=1)
 
-y_pred = model.predict()
-print(classification_report(y_test, y_pred))
+#y_pred = model.predict()
+#print(classification_report(y_test, y_pred))
