2d0cf316c6079276ec89c806ba6ab12bd99057d7
Janis Pagel <janis.pagel@uni-koeln.de>
2024-11-27 13:28:56 +0100

    Add solution

---
+++ evaluation_solution.py
@@ -0,0 +1,43 @@
+import matplotlib.pyplot as plt
+import pandas as pd
+import numpy as np
+from tensorflow.keras import Sequential
+from tensorflow.keras.layers import Embedding, Flatten, Input, Dense
+from tensorflow.keras.preprocessing.text import Tokenizer
+from tensorflow.keras.preprocessing.sequence import pad_sequences
+from tensorflow.keras.optimizers import SGD
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import classification_report, roc_auc_score
+
+df = pd.read_csv("data.tsv", sep="\t")
+df = df.head(1000)
+X = df.Abstract
+y = df.DomainID
+number_classes = len(y.unique())
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.30, random_state=42, stratify=y)
+X_train = X_train.tolist()
+X_test = X_test.tolist()
+
+tokenizer = Tokenizer()
+tokenizer.fit_on_texts(X_train)
+vocab_size = len(tokenizer.word_index) + 1  # +1 to account for the padding token
+tokenized_X_train = tokenizer.texts_to_sequences(X_train)
+tokenized_X_test = tokenizer.texts_to_sequences(X_test)
+MAX_LENGTH = max(len(tokenized_text) for tokenized_text in tokenized_X_train)
+tokenized_X_train = pad_sequences(tokenized_X_train, maxlen=MAX_LENGTH, padding="post")
+tokenized_X_test = pad_sequences(tokenized_X_test, maxlen=MAX_LENGTH, padding="post")
+
+model = Sequential()
+model.add(Input(shape=(MAX_LENGTH,)))
+model.add(Embedding(vocab_size, 300, input_length=MAX_LENGTH))
+model.add(Flatten())
+model.add(Dense(100, activation="relu"))
+model.add(Dense(100, activation="relu"))
+model.add(Dense(number_classes, activation="softmax"))
+model.compile(loss="crossentropy", optimizer=SGD(learning_rate=0.00001))
+model.summary()
+model.fit(tokenized_X_train, y_train, epochs=20, verbose=1)
+
+y_pred = model.predict(tokenized_X_test)
+y_pred = y_pred.argmax(axis=1)
+print(classification_report(y_test, y_pred))
